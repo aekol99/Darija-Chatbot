b4bcfcfc2a9ae479ccad0dbdbb00d56d3420de5f
aekol99 <121446742+aekol99@users.noreply.github.com>
2023-03-15 10:05:02 +0100

    changing q/e from english to darija

--- frontend/src/components/questionsMenu/QuestionsMenu.js
+++ frontend/src/components/questionsMenu/QuestionsMenu.js
@@ -5,15 +5,15 @@ function QuestionsMenu() {
   return (
     <div className='questions-menu'>
         <div className='alert'>
-            This is a list of questions/expressions that you can ask the bot, <span>your question/expression should be in "Darija" and it should not necessary be spelled the correctly.</span><br />
-            Exemple: who are you?  = chkon nta?, xkon nta?, ...
+            This is a list of questions/expressions that you can ask the bot, <span>your question/expression should be in "Darija" and it should not necessary be spelled the same.</span><br />
+            Exemple: chkon nta?, xkon nta?, ...
         </div>
         <ul className='questions'>
-            <li>Hello</li>
-            <li>How are you</li>
-            <li>Goodbye</li>
-            <li>What can you do</li>
-            <li>Thank you</li>
+            <li>Salam</li>
+            <li>chkon nta</li>
+            <li>bslama</li>
+            <li>ach kat3ref dir</li>
+            <li>chokran</li>
         </ul>
         More q/e will be added in the future
     </div>
